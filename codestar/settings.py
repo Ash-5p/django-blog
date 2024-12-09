ed36a2cc14ae54072654b610f3bfa979efd3601b
Ashley Preece <ashleyjph@gmail.com>
2024-12-09 18:18:18 +0000

    Ready code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-w$pn+1y@*)jes$z)o1emk@-u1odfm=gzz@p7@57i(*h3r&d1@o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ash5p-djangoblog-4dlwmkn2cna.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ash5p-djangoblog-4dlwmkn2cna.ws.codeinstitute-ide.net', 
+                '.herokuapp.com']
 
 
 # Application definition
